7e92839558b84032a5e32f722d728060eac21b55
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-11-13 21:30:04 +0100

    fixed bugs

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(cors({
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 }));
 
+
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 //   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
